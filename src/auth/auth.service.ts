01048f2602ef5872603e9b4a33be50c6caa03a3e
GezMin <s.shvetsov.v@gmail.com>
2024-05-29 19:45:51 +0500

    added service register and install argon2, faker, class-transformer, class-validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { AuthDto } from './dto/auth.dto'
+import { faker } from '@faker-js/faker'
+import { hash } from 'argon2'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private prisma: PrismaService) {}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+		if (oldUser) throw new BadRequestException('User already exists')
+
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				name: faker.person.firstName(),
+				avatarPath: faker.image.avatar(),
+				phone: faker.phone.number(),
+				password: await hash(dto.password)
+			}
+		})
+		return user
+	}
+}

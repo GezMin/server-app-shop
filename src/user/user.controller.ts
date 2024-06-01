bf64fbae2601d60b506db66428c67e08919de929
GezMin <s.shvetsov.v@gmail.com>
2024-06-01 21:39:43 +0500

    created method user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, HttpCode, Param, Patch } from '@nestjs/common'
 
-@Controller('user')
+import { UserService } from './user.service'
+import { Auth } from 'src/auth/decorators/auth.decorators'
+import { CurrentUser } from 'src/auth/decorators/user.decorators'
+
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: string) {
+		return this.userService.getById(id)
+	}
+
+	@HttpCode(200)
+	@Auth()
+	@Patch('profile/favorites/:productId')
+	async toggleFavorite(
+		@CurrentUser('id') id: string,
+		@Param('productId') productId: string
+	) {
+		return this.userService.toggleFavorite(id, productId)
+	}
 }

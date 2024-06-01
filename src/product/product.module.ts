694cb0f55581463c974d9fce04752af86e98f2b1
GezMin <s.shvetsov.v@gmail.com>
2024-06-01 21:02:28 +0500

    fix bug in module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,5 +1,4 @@
 import { CategoryService } from './../category/category.service'
-import { CategoryController } from './../category/category.controller'
 import { Module } from '@nestjs/common'
 import { ProductService } from './product.service'
 import { ProductController } from './product.controller'
@@ -7,7 +6,6 @@ import { PrismaService } from 'src/prisma.service'
 
 @Module({
 	controllers: [ProductController],
-	providers: [ProductService, PrismaService, CategoryController],
-	exports: [CategoryService]
+	providers: [ProductService, PrismaService, CategoryService]
 })
 export class ProductModule {}

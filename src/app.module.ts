f4de6c2957bf17ce805e9fedd5fc2c9a43f26f9b
GezMin <s.shvetsov.v@gmail.com>
2024-06-09 21:22:29 +0500

    added @nestjs/serve-static and app-root-path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { path } from 'app-root-path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { ConfigModule } from '@nestjs/config'
-import { CategoryModule } from './category/category.module';
-import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module'
+import { ProductModule } from './product/product.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, CategoryModule, ProductModule, UserModule],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		CategoryModule,
+		ProductModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
